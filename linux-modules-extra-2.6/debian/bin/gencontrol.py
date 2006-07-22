a49fcd022434d4c3f8a51b9349ebab09880bde72
waldi <waldi@510b9475-24dd-0310-9b6c-e0eefe99d49f>
2006-07-22 16:53:12 +0000

    /dists/trunk/linux-modules-extra-2.6: Copy from /people/waldi/linux-modules-extra-2.6.
    git-svn-id: svn://svn.debian.org/svn/kernel/dists/trunk@7067 510b9475-24dd-0310-9b6c-e0eefe99d49f

---
+++ linux-modules-extra-2.6/debian/bin/gencontrol.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python2.4
+import sys
+sys.path.append(sys.argv[1] + "/lib/python")
+import debian_linux.gencontrol
+from debian_linux import config
+from debian_linux.debian import *
+
+class gencontrol(debian_linux.gencontrol.gencontrol):
+    def __init__(self, configdir):
+        super(gencontrol, self).__init__(configdir)
+        self.process_config_version(config.config_parser({}, [sys.argv[1] + "/version"]))
+        self.config = config_reader_modules(self.config)
+
+    def do_main_packages(self, packages, extra):
+        vars = self.vars
+
+        packages['source']['Build-Depends'].extend(
+            ['linux-support-%s%s' % (self.version['upstream'], self.abiname)]
+        )
+        packages['source']['Build-Depends'].extend(
+            ['linux-headers-%s%s-all-%s [%s]' % (self.version['upstream'], self.abiname, arch, arch)
+            for arch in self.config['base',]['arches']],
+        )
+
+        for module in iter(self.config['base',]['modules']):
+            packages['source']['Build-Depends'].append('%s-source' % module)
+
+    def do_flavour(self, packages, makefile, arch, subarch, flavour, vars, makeflags, extra):
+        config_entry = self.config.merge('base', None, arch, subarch, flavour)
+        if config_entry.get('modules', True) is False:
+            return
+
+        super(gencontrol, self).do_flavour(packages, makefile, arch, subarch, flavour, vars, makeflags, extra)
+
+        for module in iter(self.config['base',]['modules']):
+            self.do_module(module, packages, makefile, arch, subarch, flavour, vars.copy(), makeflags.copy(), extra)
+
+    def do_flavour_makefile(self, makefile, arch, subarch, flavour, makeflags, extra):
+        for i in self.makefile_targets:
+            makefile.append("%s-%s-%s:: %s-%s-%s-%s" % (i, arch, subarch, i, arch, subarch, flavour))
+
+    def do_module(self, module, packages, makefile, arch, subarch, flavour, vars, makeflags, extra):
+        config_entry = self.config['base', module]
+        vars.update(config_entry)
+        vars['module'] = module
+        makeflags['MODULE'] = module
+
+        if not vars.get('longdesc', None):
+            vars['longdesc'] = ''
+
+        if arch not in config_entry.get('arches', [arch]):
+            return
+        if subarch not in config_entry.get('subarches', [subarch]):
+            return
+
+        modules = self.templates["control.modules"]
+        modules = self.process_packages(modules, vars)
+
+        for package in modules:
+            name = package['Package']
+            if packages.has_key(name):
+                package = packages.get(name)
+                package['Architecture'].append(arch)
+            else:
+                package['Architecture'] = [arch]
+                packages.append(package)
+
+        packages.extend(modules)
+
+        makeflags_string = ' '.join(["%s='%s'" % i for i in makeflags.iteritems()])
+
+        cmds_binary_arch = []
+        cmds_binary_arch.append(("$(MAKE) -f debian/rules.real binary-arch %s" % makeflags_string,))
+        cmds_build = []
+        cmds_build.append(("$(MAKE) -f debian/rules.real build %s" % makeflags_string,))
+        cmds_setup = []
+        cmds_setup.append(("$(MAKE) -f debian/rules.real setup %s" % makeflags_string,))
+        for i in self.makefile_targets:
+            makefile.append("%s-%s-%s-%s:: %s-%s-%s-%s-%s" % (i, arch, subarch, flavour, i, arch, subarch, flavour, module))
+        makefile.append(("binary-arch-%s-%s-%s-%s:" % (arch, subarch, flavour, module), cmds_binary_arch))
+        makefile.append(("build-%s-%s-%s-%s:" % (arch, subarch, flavour, module), cmds_build))
+        makefile.append(("setup-%s-%s-%s-%s:" % (arch, subarch, flavour, module), cmds_setup))
+
+    def process_config_version(self, config):
+        entry = config['version',]
+        self.process_version(parse_version(entry['source']))
+        self.vars['abiname'] = self.abiname = entry['abiname']
+
+class config_reader_modules(config.config_reader):
+    schema_base = {
+        'modules': config.schema_item_list(),
+    }
+
+    schema_module = {
+        'arches': config.schema_item_list(),
+    }
+
+    def __init__(self, arch):
+        super(config_reader_modules, self).__init__(['.'])
+        self._read_base()
+
+        for section in iter(arch):
+            s1 = self.get(section, {})
+            s2 = arch[section].copy()
+            s2.update(s1)
+            self[section] = s2
+
+    def _read_base(self):
+        config_file = config.config_parser(self.schema_base, self._get_files(self.config_name))
+
+        modules = config_file['base',]['modules']
+
+        for section in iter(config_file):
+            real = list(section)
+            if real[-1] in modules:
+                real.insert(0, 'base')
+            else:
+                real.insert(0, real.pop())
+            self[tuple(real)] = config_file[section]
+
+        for module in modules:
+            self._read_module(module)
+
+    def _read_module(self, module):
+        config_file = config.config_parser(self.schema_base, self._get_files("%s/%s" % (module, self.config_name)))
+
+        for section in iter(config_file):
+            real = list(section)
+            real[0:] = [real.pop(), module]
+            real = tuple(real)
+            s = self.get(real, {})
+            s.update(config_file[section])
+            self[real] = s
+
+    def merge(self, section, module, *args):
+        ret = {}
+        ret.update(super(config_reader_modules, self).merge(section, *args))
+        if module:
+            ret.update(super(config_reader_modules, self).merge(section, module, *args))
+        return ret
+
+if __name__ == '__main__':
+    gencontrol(sys.argv[1] + "/arch")()
