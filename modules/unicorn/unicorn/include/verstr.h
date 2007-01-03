65f9aba2a84465f2a29d924de6a04e89d788a2d5
waldi <waldi@510b9475-24dd-0310-9b6c-e0eefe99d49f>
2007-01-03 22:49:27 +0000

    /dists/trunk/modules/fusion/linux-fusion-1.1.orig.tar.gz, /dists/trunk/modules/unicorn: Remove. This is no place to store blobs and other external stuff.
    git-svn-id: svn://svn.debian.org/svn/kernel/dists/trunk@8081 510b9475-24dd-0310-9b6c-e0eefe99d49f

--- modules/unicorn/unicorn/include/verstr.h
+++
@@ -1,3 +0,0 @@
-#ifndef VERSTR
-#define VERSTR "$Revision: 0.5.0 $ time " __TIME__ " " __DATE__
-#endif
--- modules/unicorn/unicorn/msw/.crc.o.cmd
+++
@@ -1,9 +0,0 @@
-cmd_/local/home/rzr/home/src/unicorn/unicorn/unicorn/unicorn_usb/../msw/crc.o := gcc -Wp,-MD,/local/home/rzr/home/src/unicorn/unicorn/unicorn/unicorn_usb/../msw/.crc.o.d  -nostdinc -isystem /usr/lib/gcc/i486-linux-gnu/4.1.2/include -D__KERNEL__ -Iinclude  -include include/linux/autoconf.h -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs -fno-strict-aliasing -fno-common -Os -pipe -msoft-float -mpreferred-stack-boundary=2  -march=athlon  -ffreestanding -maccumulate-outgoing-args -DCONFIG_AS_CFI=1 -DCONFIG_AS_CFI_SIGNAL_FRAME=1 -Iinclude/asm-i386/mach-default -fomit-frame-pointer  -fno-stack-protector -Wdeclaration-after-statement -Wno-pointer-sign -DVERS=0x093 -D_USB_DRIVER -DDEBUG=1 -I/local/home/rzr/home/src/unicorn/unicorn/unicorn/unicorn_usb/../include -I/local/home/rzr/home/src/unicorn/unicorn/unicorn/unicorn_usb/../   -DMODULE -D"KBUILD_STR(s)=\#s" -D"KBUILD_BASENAME=KBUILD_STR(crc)"  -c -o /local/home/rzr/home/src/unicorn/unicorn/unicorn/unicorn_usb/../msw/crc.o /local/home/rzr/home/src/unicorn/unicorn/unicorn/unicorn_usb/../msw/crc.c
-
-deps_/local/home/rzr/home/src/unicorn/unicorn/unicorn/unicorn_usb/../msw/crc.o := \
-  /local/home/rzr/home/src/unicorn/unicorn/unicorn/unicorn_usb/../msw/crc.c \
-  /local/home/rzr/home/src/unicorn/unicorn/unicorn/unicorn_usb/../include/crc.h \
-
-/local/home/rzr/home/src/unicorn/unicorn/unicorn/unicorn_usb/../msw/crc.o: $(deps_/local/home/rzr/home/src/unicorn/unicorn/unicorn/unicorn_usb/../msw/crc.o)
-
-$(deps_/local/home/rzr/home/src/unicorn/unicorn/unicorn/unicorn_usb/../msw/crc.o):
