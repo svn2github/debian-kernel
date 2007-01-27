35a81db54f8ddafde6cdbff1e637c462813656c8
waldi <waldi@510b9475-24dd-0310-9b6c-e0eefe99d49f>
2007-01-27 19:17:43 +0000

    debian/bin/gencontrol.py: Fix version.
    git-svn-id: svn://svn.debian.org/svn/kernel/dists/trunk@8222 510b9475-24dd-0310-9b6c-e0eefe99d49f

--- linux-modules-contrib-2.6/debian/bin/gencontrol.py
+++ linux-modules-contrib-2.6/debian/bin/gencontrol.py
@@ -14,8 +14,8 @@ class gencontrol(debian_linux.gencontrol.gencontrol):
     def do_main_setup(self, vars, makeflags, extra):
         super(gencontrol, self).do_main_setup(vars, makeflags, extra)
         makeflags.update({
-            'VERSION_SOURCE': self.version['linux']['source_upstream'],
-            'VERSION_DEBIAN': self.version['linux']['debian'],
+            'VERSION_SOURCE': self.version['upstream'],
+            'VERSION_DEBIAN': self.version['debian'],
         })
 
     def do_main_makefile(self, makefile, makeflags, extra):
